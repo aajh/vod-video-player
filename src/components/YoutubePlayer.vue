60e31a20fa17064dbf7a9b82aabc066de6419a77
Aapo Hanski <aapo.hanski@gmail.com>
2025-01-03 12:25:08 +0200

    Working YoutubePlayer

---
+++ src/components/YoutubePlayer.vue
@@ -0,0 +1,130 @@
+<script lang="ts">
+export enum PlayerState {
+    Unstarted = -1,
+    Ended = 0,
+    Playing = 1,
+    Paused = 2,
+    Buffering = 3,
+    Cued = 5,
+};
+
+const YOUTUBE_IFRAME_API_URL =  'https://www.youtube.com/iframe_api';
+const youtubeIframeApiPromise: Promise<void> = new Promise(resolve => {
+    if (window.YT) {
+        resolve();
+        return;
+    }
+
+    const oldOnReady = window.onYouTubeIframeAPIReady;
+    window.onYouTubeIframeAPIReady = () => {
+        resolve();
+        if (typeof oldOnReady === 'function') {
+            oldOnReady();
+        }
+    }
+
+    const tag = document.createElement('script');
+    tag.src = YOUTUBE_IFRAME_API_URL;
+
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    if (firstScriptTag?.parentNode) {
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    } else {
+        document.append(tag);
+    }
+});
+</script>
+<script setup lang="ts">
+import { onMounted, watch } from 'vue';
+
+const props = defineProps<{
+    elementId: string,
+    width: number,
+    height: number,
+    videoId: string,
+}>();
+
+const state = defineModel<PlayerState>('state', { default: PlayerState.Unstarted });
+
+defineExpose({
+    getCurrentTime() {
+        if (!player) {
+            return 0;
+        }
+
+        return player.getCurrentTime();
+    },
+    play() {
+        if (!player) {
+            return;
+        }
+
+        player.playVideo();
+    },
+    pause() {
+        if (!player) {
+            return;
+        }
+
+        player.pauseVideo();
+    },
+    stop() {
+        if (!player) {
+            return;
+        }
+
+        player.stopVideo();
+    },
+    seekTo(time: number) {
+        if (!player) {
+            return;
+        }
+
+        player.seekTo(time, true);
+    },
+});
+
+
+let player: YT.Player | null = null;
+let videoIdToBeQueued: string | null = null;
+onMounted(async () => {
+    await youtubeIframeApiPromise;
+
+    const p = new YT.Player(props.elementId, {
+        height: props.height,
+        width: props.width,
+        playerVars: {
+            playsinline: 1,
+        },
+        events: {
+            onReady: () => {
+                player = p;
+                if (videoIdToBeQueued) {
+                    player.cueVideoById(videoIdToBeQueued);
+                }
+            },
+            onStateChange: e => {
+                state.value = e.data;
+            },
+        },
+    });
+});
+
+watch(() => props.videoId, (newVideoId, oldVideoId) => {
+    if (newVideoId === oldVideoId) {
+        return;
+    }
+
+    if (player) {
+        player.cueVideoById(newVideoId);
+    } else {
+        videoIdToBeQueued = newVideoId;
+    }
+}, {
+    immediate: true,
+});
+</script>
+
+<template>
+    <div :id="elementId"></div>
+</template>
--- src/views/WatchView.vue
+++ src/views/WatchView.vue
@@ -1,6 +1,49 @@
+<script setup lang="ts">
+import { computed, ref, useTemplateRef } from 'vue';
+import YoutubePlayer from '@/components/YoutubePlayer.vue';
+import { PlayerState } from '@/components/YoutubePlayer.vue';
+
+const running = ref(false);
+
+const vodPlayer = useTemplateRef<typeof YoutubePlayer>('vod-player');
+const vodPlayerState = ref(PlayerState.Unstarted);
+const vodPlayerPlaying = computed(() => vodPlayerState.value === PlayerState.Playing);
+
+function play() {
+    if (!vodPlayer.value) {
+        return;
+    }
+
+    running.value = true;
+    vodPlayer.value.play();
+}
+
+function pause() {
+    if (!vodPlayer.value) {
+        return;
+    }
+
+    running.value = false;
+    vodPlayer.value.pause();
+}
+</script>
+
 <template>
     <div>
         <h1>This is the watch page</h1>
+        <YoutubePlayer
+            ref="vod-player"
+            v-model:state="vodPlayerState"
+            element-id="vod-player"
+            :width="320"
+            :height="195"
+            video-id="Q3Kvu6Kgp88"
+        />
+        <button v-show="!running" type="button" @click="play">Play</button>
+        <button v-show="running" type="button" @click="pause">Pause</button>
+        <div>
+            VOD player state {{PlayerState[vodPlayerState]}} {{vodPlayerPlaying}}
+        </div>
     </div>
 </template>
 
